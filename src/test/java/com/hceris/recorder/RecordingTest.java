bb3ef53fb2f45b1392deae6f00ec156b5eb7ff5c
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-09-09 01:17:29 +0200

    Custom config for the recordings

--- src/test/java/com/hceris/recorder/RecordingTest.java
+++ src/test/java/com/hceris/recorder/RecordingTest.java
@@ -1,9 +1,12 @@
 package com.hceris.recorder;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.recording.RecordSpec;
 import org.junit.After;
 import org.junit.Before;
 import org.springframework.beans.factory.annotation.Value;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.recordSpec;
 
 public abstract class RecordingTest {
     @Value("${recordingServerUrl}")
@@ -15,12 +18,19 @@ public abstract class RecordingTest {
     public void setUp() throws Exception {
         server = new WireMockServer(8081);
         server.start();
-        server.startRecording(recordingServerUrl);
+        server.startRecording(config());
     }
 
     @After
     public void tearDown() throws Exception {
         server.stopRecording();
         server.stop();
+    }
+
+    private RecordSpec config() {
+        return recordSpec()
+                .forTarget(recordingServerUrl)
+                .makeStubsPersistent(false)
+                .build();
     }
 }
