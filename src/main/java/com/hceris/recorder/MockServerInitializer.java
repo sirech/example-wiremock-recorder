a87d526bbc4aab5daccd74dfa1c1562372aaa60c
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-09-09 12:31:57 +0200

    Start mock server automatically for development profile

---
+++ src/main/java/com/hceris/recorder/MockServerInitializer.java
@@ -0,0 +1,24 @@
+package com.hceris.recorder;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MockServerInitializer {
+    @Value("${mock.active}")
+    boolean active;
+
+    @Value("${mock.port}")
+    int port;
+
+    @EventListener
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if(active) {
+            WireMockServer server = new WireMockServer(port);
+            server.start();
+        }
+    }
+}
