2d09565772cef4e5405421e5f4e60ed6b5dfb72a
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-08-05 22:12:25 +0200

    fix jackson and lombok not playing with each other

--- src/main/java/com/hceris/recorder/Todo.java
+++ src/main/java/com/hceris/recorder/Todo.java
@@ -1,12 +1,19 @@
 package com.hceris.recorder;
 
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.NonNull;
 import lombok.Value;
 
 @Value
-@Builder
 public class Todo {
+    @JsonCreator
+            public static Todo create(@JsonProperty("userId") int userId,
+                                      @JsonProperty("title") String title,
+                                      @JsonProperty("completed") boolean completed) {
+        return new Todo(userId, title, completed);
+    }
+
     int userId;
     @NonNull
     String title;
